affa456139364aacc5381a9fb48521c347bb3691
Kevin <ngynkvn@gmail.com>
2024-10-25 15:35:11 -0400

    chore: basic example

---
+++ example/src/main.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var rb = try @import("ring-buffer").RingBuffer(f32).init(std.heap.page_allocator, 4);
+    try rb.write(1);
+    try rb.write(1);
+    try rb.write(2);
+    try rb.write(3);
+    rb.write(3) catch rb.writeAssumeCapacity(4);
+    std.debug.print("{d}\n", .{rb.data});
+}
